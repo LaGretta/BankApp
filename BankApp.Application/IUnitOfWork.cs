06a650036766b430b690724c98aaf24607b4ee73
LaGretta <sashagutsul2014@gmail.com>
2026-08-06 04:10:26 +0200

    Add transaction methods to unit of work interface

--- BankApp.Application/IUnitOfWork.cs
+++ BankApp.Application/IUnitOfWork.cs
@@ -3,4 +3,7 @@
 public interface IUnitOfWork
 {
     Task<int> SaveChangesAsync(CancellationToken ct);
+    Task BeginTransactionAsync(CancellationToken ct);
+    Task CommitTransactionAsync(CancellationToken ct);
+    Task RollbackTransactionAsync(CancellationToken ct);
 }
