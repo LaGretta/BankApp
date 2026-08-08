1ef9255b125933e6644554dbf906ce377f76cfeb
LaGretta <sashagutsul2014@gmail.com>
2026-08-08 20:02:02 +0200

    Add infrastructure DI, configure Program.cs with JWT and Serilog

---
+++ BankApp.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,34 @@
+﻿using BankApp.Application;
+using BankApp.Application.Interfaces;
+using BankApp.Application.Interfaces.Repository;
+using BankApp.Application.Interfaces.Security;
+using BankApp.Infrastructure.Data;
+using BankApp.Infrastructure.Repository;
+using BankApp.Infrastructure.Security;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BankApp.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<BankDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+        services.AddScoped<IAuthRepository, AuthRepository>();
+        services.AddScoped<IAccountRepository, AccountRepository>();
+        services.AddScoped<ICardRepository, CardRepository>();
+        services.AddScoped<ITransactionRepository, TransactionRepository>();
+
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+        services.AddScoped<IPasswordHasher, PasswordHasher>();
+        services.AddScoped<IJwtTokenGenerator, JwtTokenGenerator>();
+        services.AddScoped<ICardNumberGenerator, CardNumberGenerator>();
+
+        return services;
+    }
+}
