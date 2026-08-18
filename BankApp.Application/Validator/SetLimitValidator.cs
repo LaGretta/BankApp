336854bd74afd776e861640e2d8693c1818b9170
LaGretta <sashagutsul2014@gmail.com>
2026-08-18 20:03:26 +0200

    Wire up FluentValidation via action filter; add SetLimitDto validator

---
+++ BankApp.Application/Validator/SetLimitValidator.cs
@@ -0,0 +1,14 @@
+﻿using BankApp.Application.DTO;
+using FluentValidation;
+
+namespace BankApp.Application.Validator;
+
+public class SetLimitDtoValidator : AbstractValidator<SetLimitDto>
+{
+    public SetLimitDtoValidator()
+    {
+        RuleFor(x => x.DailyLimit)
+            .GreaterThan(0).When(x => x.DailyLimit.HasValue)
+            .WithMessage("Limit must be positive");
+    }
+}
